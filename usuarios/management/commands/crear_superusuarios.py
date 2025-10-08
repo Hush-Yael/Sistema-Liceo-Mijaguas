b92d45ad7de3967a2399113cf2de02a0dceb1857
Yael <srjean21@gmail.com>
2025-10-07 21:45:19 -0400

    nuevo: se añadió un comando a manage para crear un superusuario

---
+++ usuarios/management/commands/crear_superusuarios.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        from usuarios.models import User
+
+        superuser = User.objects.filter(is_superuser=True).first()
+        print(superuser)
+        if superuser is None:
+            User.objects.create_superuser("admin", "", "admin")
