52f01c427a04b21175c1df772620666950688f0f
Yael <srjean21@gmail.com>
2025-10-11 21:08:29 -0400

    se cambiaron los campos que se muestran en la tabla de los usuarios

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,4 +2,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from usuarios.models import User
 
+UserAdmin.list_display = ["username", "email", "is_staff", "is_active"]
+UserAdmin.search_fields = ["username", "email"]
+
+UserAdmin.fieldsets = (
+    (None, {"fields": ("username", "password")}),
+    (("Información personal"), {"fields": ("email",)}),
+    (
+        ("Permisos"),
+        {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            ),
+        },
+    ),
+    (("Fechas importantes"), {"fields": ("last_login", "date_joined")}),
+)
+
 admin.site.register(User, UserAdmin)
