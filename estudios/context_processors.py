c757d8a30da0bbadaae87e59f718b3a081d55ebd
Yael <srjean21@gmail.com>
2025-10-13 17:31:07 -0400

    cambios: se eliminó el contexto que contenía los AÑOS y MATERIAS estáticos, ya que no se usaban. Aparte, se movió el que tiene DEBUG a la carpeta principal de la app

--- estudios/context_processors.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpRequest
-from estudios.management.commands.poblar_datos_estudios import AÑOS, MATERIAS
-
-
-def contexto(request: HttpRequest):
-    return {"AÑOS": AÑOS, "MATERIAS": MATERIAS}
