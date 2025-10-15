2cd7fda82f2b5531c3dd551560da56cb709f45d3
Yael <srjean21@gmail.com>
2025-10-14 21:54:59 -0400

    nuevo: ahora se pueden filtrar los lapsos por su número

--- estudios/admin.py
+++ estudios/admin.py
@@ -68,6 +68,7 @@ class EstudianteAdmin(ModelAdmin):
 @admin.register(Lapso)
 class LapsoAdmin(ModelAdmin):
     list_display = ["numero_lapso", "nombre_lapso", "fecha_inicio", "fecha_fin"]
+    list_filter = ["numero_lapso"]
     search_fields = ["nombre_lapso"]
 
 
