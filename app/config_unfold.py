c5bcc97cc118c834bf213f736c64f98c83eaeb08
Yael <srjean21@gmail.com>
2025-10-20 13:47:45 -0400

    cambios: se eliminó el logo de la sidebar, porque no había manera que saliera el texto del header, entonces se dejó solo este último

--- app/config_unfold.py
+++ app/config_unfold.py
@@ -3,7 +3,6 @@ from django.templatetags.static import static
 CONFIG = {
     "SITE_TITLE": "Panel de administración - Liceo Mijaguas",
     "SITE_HEADER": "Panel de administración",
-    "SITE_LOGO": lambda _: static("img/logo-sin-letras.png"),
     "SITE_FAVICONS": [
         {
             "rel": "icon",
