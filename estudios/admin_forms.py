e9aaa93820e9e2ada849144a3f4b995543a2ba4f
Yael <srjean21@gmail.com>
2025-10-20 22:03:09 -0400

    nuevo: ahora se valida que al matricular, no se exceda la capacidad máxima de la sección seleccionada

--- estudios/admin_forms.py
+++ estudios/admin_forms.py
@@ -31,6 +31,20 @@ class MatriculaAdminForm(forms.ModelForm):
 
         return estudiante_cedula
 
+    def clean_seccion(self):
+        seccion = self.cleaned_data.get("seccion")
+
+        if seccion is not None:
+            cantidad_maxima = Seccion.objects.get(id=seccion.id).capacidad_maxima
+            cantidad_actual = Matricula.objects.filter(seccion=seccion).count()
+
+            if cantidad_actual >= cantidad_maxima:
+                raise forms.ValidationError(
+                    f"La sección seleccionada se encuentra llena (capacidad máxima: {cantidad_maxima})"
+                )
+
+        return seccion
+
 
 class LapsoAdminForm(forms.ModelForm):
     class Meta:
