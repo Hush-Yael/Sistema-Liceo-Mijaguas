3f75ccb4b4e9dedf31b502b4d8dcdce77a753e10
Jean <srjean21@gmail.com>
2025-12-14 12:58:08 -0400

    nuevo: se evita el cache en dev

---
+++ app/middleware.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+
+class StaticUnCachingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if settings.DEBUG and request.path.startswith("/static"):
+            response.headers["Cache-Control"] = "max-age=0"
+        return response
