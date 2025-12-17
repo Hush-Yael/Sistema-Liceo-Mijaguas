4187e2789ee2131d22e4fffe953a3d50c37c3cd6
Jean <srjean21@gmail.com>
2025-12-17 13:45:56 -0400

    nuevo: se añadieron utilidades para usar "querySelectorAll"

--- global.d.ts
+++ global.d.ts
@@ -3,8 +3,10 @@ import type { Van } from "./static/js/van-1.6.0";
 declare global {
   const van: Van;
   const $: typeof document.querySelector;
+  const $$: typeof document.querySelectorAll;
   const $id: typeof document.getElementById;
   interface HTMLElement {
     $: typeof document.querySelector;
+    $$: typeof document.querySelectorAll;
   }
 }
--- static/js/utilidades.js
+++ static/js/utilidades.js
@@ -1,10 +1,18 @@
 Object.defineProperties(window, {
   $: { value: (query) => document.querySelector(query) },
+  $$: { value: (query) => document.querySelectorAll(query) },
   $id: { value: (id) => document.getElementById(id) },
 });
 
-Object.defineProperty(HTMLElement.prototype, "$", {
-  value: function (query) {
-    return this.querySelector(query);
+Object.defineProperties(HTMLElement.prototype, {
+  $: {
+    value: function (query) {
+      return this.querySelector(query);
+    },
+  },
+  $$: {
+    value: function (query) {
+      return this.querySelectorAll(query);
+    },
   },
 });
