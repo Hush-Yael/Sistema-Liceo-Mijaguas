4fd1c63c2f2b78e5eb55d4aaf38bc4e5374d4422
Jean <srjean21@gmail.com>
2025-12-17 15:13:25 -0400

    cambios y nuevo: se adaptó para usar un mapa en vez de un array para "inputsLimpiados", y ahora se eliminan también los errores al reiniciar los formularios

--- static/js/limpiar-errores.js
+++ static/js/limpiar-errores.js
@@ -1,12 +1,12 @@
 let valorErrorActual = "";
 
-/** @type {{input: HTMLInputElement, error: string}[]} */
-const inputsLimpiados = [];
+/** @type {Map<string, { input: HTMLInputElement, valorError: string | unknown }>} */
+const inputsLimpiados = new Map();
 
 $$("form").forEach((form) => {
   form.addEventListener("focus", rastrearErrorInputActual, true);
-
   form.onchange = limpiarAlCambiar;
+  form.onreset = limpiarAlReiniciar;
 });
 
 // guarda el valor erróneo del input al momento de enfocarlo, para verificar cuando se vuelva a introducir o desenfocar
@@ -24,24 +24,57 @@ function rastrearErrorInputActual(e) {
 // si se vuelve a introducir el valor erróneo en el mismo campo, volver a mostrar el error
 /** @param {Event} e */
 function limpiarAlCambiar(e) {
-  /** @type {HTMLElement} */
-  const elemento = e.target;
-  const input = elemento.matches("input");
+  if (e.target !== e.currentTarget) {
+    /** @type {HTMLInputElement} */
+    const campo = e.target;
 
-  if (input) {
     // guardar el valor erróneo del input
-    if (elemento.hasAttribute("aria-invalid")) {
-      elemento.removeAttribute("aria-invalid");
-      inputsLimpiados.push({ input, valorError: valorErrorActual });
+    if (campo.hasAttribute("aria-invalid")) {
+      campo.removeAttribute("aria-invalid");
+      inputsLimpiados.set(campo.id, {
+        campo,
+        valorError: valorErrorActual,
+      });
     } else {
-      const limpiado = inputsLimpiados.find((l) => l.input === input);
+      const limpiado = inputsLimpiados.get(campo.id);
 
       // si se vuelve a introducir el valor erróneo en el mismo campo, volver a mostrar el error
-      if (limpiado && limpiado.valorError === elemento.value) {
-        elemento.setAttribute("aria-invalid", "true");
-        elemento.setAttribute("aria-errormessage", "error");
-        inputsLimpiados.splice(inputsLimpiados.indexOf(limpiado), 1);
+      if (limpiado && limpiado.valorError === campo.value) {
+        campo.setAttribute("aria-invalid", "true");
+        campo.setAttribute("aria-errormessage", "error");
+        inputsLimpiados.delete(campo.id);
       }
     }
   }
+}
+
+// elimina los errores al reiniciar el formulario
+/** @param {Event} e */
+function limpiarAlReiniciar(e) {
+  /** @type {HTMLFormElement} */
+  const form = e.target;
+
+  /** @type {HTMLElement[]} */
+  const camposConErrores = Array.from(form.$$("input[aria-invalid='true']"));
+  const mensajesErrores = Array.from(form.$$(".input-error"));
+
+  if (form.id) {
+    const camposConErroresFuera = $$(
+      "input[aria-invalid='true'][form='" + form.id + "']",
+    );
+    camposConErroresFuera.length &&
+      camposConErrores.push(...camposConErroresFuera);
+
+    const mensajesErroresFuera = $$(`.input-error[data-form="${form.id}"]`);
+    mensajesErroresFuera.length &&
+      mensajesErrores.push(...mensajesErroresFuera);
+  }
+
+  camposConErrores.forEach((input) => {
+    input.removeAttribute("aria-invalid");
+    input.removeAttribute("aria-errormessage");
+    inputsLimpiados.delete(input.id);
+  });
+
+  mensajesErrores.forEach((mensaje) => mensaje.remove());
 }
