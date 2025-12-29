0a74c5409e2b5b4f29c4615f97d8d20007be4fb9
Jean <srjean21@gmail.com>
2025-12-29 15:15:36 -0400

    nuevo: se implementó un tipo de respuesta que permite redireccionar al hacer peticiones con HTMX

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 import re
 from django.template import base as template_base
+from django.http import HttpResponseRedirect
 
 # Soporte para etiquetas de plantilla multilínea
 template_base.tag_re = re.compile(template_base.tag_re.pattern, re.DOTALL)
+
+
+# Soporte para redirecciones con HTMX
+class HTTPResponseHXRedirect(HttpResponseRedirect):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self["HX-Redirect"] = self["Location"]
+
+    status_code = 200
