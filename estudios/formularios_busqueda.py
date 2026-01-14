e1ea7f44723d48b0b031d111b77c52c0a50f13e0
Jean <srjean21@gmail.com>
2026-01-14 12:30:57 -0400

    cambios y nuevo: se añadió un mixin para formularios que contengan campos para buscar por lapso y sección

--- estudios/formularios_busqueda.py
+++ estudios/formularios_busqueda.py
@@ -4,31 +4,33 @@ from app.forms import BusquedaFormMixin
 from .models import Lapso, Materia, Seccion
 
 
-class NotasBusquedaForm(BusquedaFormMixin):
+class LapsoYSeccionBusquedaFormMixin(BusquedaFormMixin):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.fields["secciones"].label_from_instance = lambda obj: obj.nombre  # type: ignore
         self.fields["lapsos"].label_from_instance = lambda obj: obj.nombre  # type: ignore
 
+    secciones = forms.ModelMultipleChoiceField(
+        label="Sección:",
+        queryset=Seccion.objects.all().order_by("año", "letra"),
+        required=False,
+    )
+
+    lapsos = forms.ModelMultipleChoiceField(
+        label="Lapso:",
+        queryset=Lapso.objects.all().order_by("-id"),
+        required=False,
+    )
+
+
+class NotasBusquedaForm(LapsoYSeccionBusquedaFormMixin):
     campos_prefijo_cookie = "notas"
     opciones_columna_buscar = (
         ("nombres_y_apellidos", "Nombres y apellidos"),
         ("matricula__estudiante__nombres", "Nombres"),
         ("matricula__estudiante__apellidos", "Apellidos"),
         ("matricula__estudiante__cedula", "Cédula"),
-    )
-
-    secciones = forms.ModelMultipleChoiceField(
-        label="Sección",
-        queryset=Seccion.objects.all().order_by("año", "letra"),
-        required=False,
-    )
-
-    lapsos = forms.ModelMultipleChoiceField(
-        label="Lapso",
-        queryset=Lapso.objects.all().order_by("-id"),
-        required=False,
     )
 
     materias = forms.ModelMultipleChoiceField(
