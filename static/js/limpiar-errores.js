97170d8f28f71261d89cad3dc92747f3b91df291
Jean <srjean21@gmail.com>
2026-02-06 15:11:22 -0400

    cambios y nuevo: ahora se muestra un btn para mostrar las contraseñas en caso de tratarse de ese tipo de campo

--- static/js/limpiar-errores.js
+++ static/js/limpiar-errores.js
@@ -1,6 +1,6 @@
 let valorErrorActual = "";
 
-/** @type {Map<string, { input: HTMLInputElement, valorError: string | unknown }>} */
+/** @type { Map<string, { input: HTMLInputElement, valorError: string | unknown }> } */
 const inputsLimpiados = new Map();
 
 $$("form").forEach((form) => {
@@ -21,17 +21,20 @@ function rastrearErrorInputActual(e) {
   else valorErrorActual = "";
 }
 
-// si se vuelve a introducir el valor erróneo en el mismo campo, volver a mostrar el error
+// verificar si se vuelve a introducir un valor erróneo o si se introduce uno diferente
 /** @param {Event} e */
 function limpiarAlCambiar(e) {
   if (e.target !== e.currentTarget) {
     /** @type {HTMLInputElement} */
     const campo = e.target;
 
-    // guardar el valor erróneo del input
+    // eliminar los atributos y guardar el valor erróneo anterior del input
     if (campo.hasAttribute("aria-invalid")) {
+      const wrapper = campo.closest("[data-invalido]");
+      if (wrapper) wrapper.setAttribute("data-invalido", "false");
+
       campo.removeAttribute("aria-invalid");
-      inputsLimpiados.set(campo.id, {
+      inputsLimpiados.set(campo.id || campo.name, {
         campo,
         valorError: valorErrorActual,
       });
@@ -40,6 +43,9 @@ function limpiarAlCambiar(e) {
 
       // si se vuelve a introducir el valor erróneo en el mismo campo, volver a mostrar el error
       if (limpiado && limpiado.valorError === campo.value) {
+        const wrapper = campo.closest("[data-invalido]");
+        if (wrapper) wrapper.setAttribute("data-invalido", "true");
+
         campo.setAttribute("aria-invalid", "true");
         campo.setAttribute("aria-errormessage", "error");
         inputsLimpiados.delete(campo.id);
