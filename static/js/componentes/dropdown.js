fc2140971c575446c5048026878feaf24f1d4e5e
Jean <srjean21@gmail.com>
2026-02-23 13:41:57 -0400

    cambios y nuevo: se adaptó el script para los dropdowns al convertir la fuente de datos en una clase. Además, se mejoró el ajusto del posicionamiento del menú

--- static/js/componentes/dropdown.js
+++ static/js/componentes/dropdown.js
@@ -1,164 +1,192 @@
 "use strict";
-{
-  document.addEventListener("alpine:init", () => {
-    Alpine.data("dropdown", (config) => ({
-      /** @type { boolean } */
-      abierto: config.abierto || false,
-      /** @type { boolean } */
-      abiertoPorTeclado: config.abiertoPorTeclado || false,
-      /** @type { string } */
-      menuStyle: config.menuStyle || "",
 
-      init() {
-        Alpine.bind(this.$el, {
-          "@click.outside": this.cerrarMenu,
-        });
+// oxlint-disable-next-line no-unused-vars
+const Dropdown = class {
+  constructor(config) {
+    Object.assign(this, config);
+
+    /** @type { boolean } */
+    this.abierto = config.abierto || false;
+
+    /** @type { boolean } */
+    this.abiertoPorTeclado = config.abiertoPorTeclado || false;
+
+    /** @type { string } */
+    this.posPorDefecto =
+      (config.pos_x || "") + (config.pos_y || "") ||
+      "top: 100%; margin-top: 8px; left: 0;";
+
+    this.menuStyle = this.posPorDefecto;
+
+    /** @type { string | undefined } */
+    this.pos_x = config.pos_x;
+
+    /** @type { string | undefined } */
+    this.pos_y = config.pos_y;
+
+    this.trigger = {
+      "x-ref": "trigger",
+      type: "button",
+      "aria-haspopup": config.ariaHaspopup || "true",
+      ":aria-expanded": "abierto || abiertoPorTeclado",
+      "@click"() {
+        this.abierto = !this.abierto;
+        this.$nextTick(() => this.ajustarPosicion());
       },
+      "@keydown"() {
+        /** @type { KeyboardEvent } */
+        const e = this.$event;
 
-      cerrarMenu() {
-        this.abierto = false;
-        this.abiertoPorTeclado = false;
-        this.menuStyle = "";
+        switch (e.key) {
+          case " ":
+          case "Enter":
+          case "ArrowDown": {
+            e.preventDefault();
+            this.abiertoPorTeclado = true;
+            this.$nextTick(() => this.ajustarPosicion());
+          }
+        }
       },
+    };
 
-      trigger: {
-        "x-ref": "trigger",
-        type: "button",
-        "aria-haspopup": config.ariaHaspopup || "true",
-        ":aria-expanded": "abierto || abiertoPorTeclado",
-        "@click"() {
-          this.abierto = !this.abierto;
+    this.menu = {
+      role: config.ariaHaspopup || "menu",
+      "x-trap": "abierto || abiertoPorTeclado",
+      "x-ref": "menu",
+      "@keydown"() {
+        /** @type { KeyboardEvent } e */
+        const e = this.$event;
+        const $focus = this.$focus;
 
-          if (config.noAjustable) return;
-
-          if (this.abierto) this.$nextTick(() => this.ajustarPosicion());
-          else this.menuStyle = "";
-        },
-        "@keydown"() {
-          /** @type { KeyboardEvent } */
-          const e = this.$event;
-
-          switch (e.key) {
-            case " ":
-            case "Enter":
-            case "ArrowDown": {
+        switch (e.key) {
+          case "Escape": {
+            e.stopPropagation();
+            this.abierto = false;
+            this.abiertoPorTeclado = false;
+            return this.$nextTick(() => this.$refs.trigger.focus());
+          }
+          case "ArrowDown": {
+            e.preventDefault();
+            return $focus.wrap().next();
+          }
+          case "ArrowUp": {
+            e.preventDefault();
+            return $focus.wrap().previous();
+          }
+          case "Home": {
+            if (e.target.type !== "text") {
               e.preventDefault();
-              this.abiertoPorTeclado = true;
-
-              if (config.noAjustable) return;
-              this.$nextTick(() => this.ajustarPosicion());
+              return $focus.wrap().first();
             }
           }
-        },
+          case "End": {
+            if (e.target.type !== "text") {
+              e.preventDefault();
+              return $focus.wrap().last();
+            }
+          }
+        }
+
+        this.shiftPresionado = e.shiftKey;
       },
-
-      menu: {
-        role: config.ariaHaspopup || "menu",
-        "x-trap": "abierto || abiertoPorTeclado",
-        "x-ref": "menu",
-        "@keydown": manejarTeclas,
-        "x-transition": true,
-        "x-show"() {
-          return this.abierto || this.abiertoPorTeclado;
-        },
-        ":style"() {
-          return this.menuStyle;
-        },
+      ...(config.transicionPersonalizada
+        ? undefined
+        : { "x-transition": true }),
+      "x-show"() {
+        return this.abierto || this.abiertoPorTeclado;
       },
+      ":style"() {
+        return this.menuStyle;
+      },
+    };
+  }
 
-      ajustarPosicion: config.noAjustable ? null : ajustarPosicion,
+  init() {
+    Alpine.bind(this.$el, {
+      "@click.outside": this.cerrarMenu,
+    });
+  }
 
-      ...config,
-    }));
-  });
+  cerrarMenu() {
+    this.abierto = false;
+    this.abiertoPorTeclado = false;
+  }
 
-  function ajustarPosicion() {
-    const recta = this.$refs.menu.getBoundingClientRect();
+  ajustarPosicion() {
+    clearTimeout(this.posTimeout);
 
-    // si sobresale de la ventana, ajustarlo
-    if (
-      recta.top < 0 ||
-      recta.bottom + 40 >
-        (window.innerHeight || document.documentElement.clientHeight) ||
-      recta.left < 0 ||
-      recta.right + 40 >
-        (window.innerWidth || document.documentElement.clientWidth)
-    ) {
+    if (this.abierto) {
       /** @type { HTMLButtonElement } */
-      const btn = this.$refs.trigger;
-      const rectaBtn = btn.getBoundingClientRect();
+      const button = this.$refs.trigger;
+      /** @type { HTMLDivElement } */
+      const dropdown = this.$refs.menu;
 
-      const menu = this.$refs.menu;
-      const ventanaAncho = window.innerWidth;
-      const ventanaAlto = window.innerHeight;
+      if (!button || !dropdown) return;
 
-      let top = rectaBtn.bottom + window.scrollY;
-      let left = rectaBtn.left + window.scrollX;
+      // Obtener dimensiones y posiciones
+      const buttonRect = button.getBoundingClientRect();
+      const dropdownRect = dropdown.getBoundingClientRect();
 
-      // Ajustar verticalmente
-      const espacioAbajo = ventanaAlto - rectaBtn.bottom;
-      const alturaMenu = menu.offsetHeight || 300; // altura estimada
+      // Determinar posición vertical
+      let style = "";
+      let verticalFlip = ""; // Por defecto abajo
+      const spaceBelow = window.innerHeight - buttonRect.bottom - 15;
+      const spaceAbove = buttonRect.top - 15;
 
-      // Mostrar arriba si hay más espacio
-      if (espacioAbajo < alturaMenu && rectaBtn.top > alturaMenu)
-        top = rectaBtn.top + window.scrollY - alturaMenu;
-
-      // Ajustar horizontalmente
-      const espacioRight = ventanaAncho - rectaBtn.left;
-      const anchoMenu = menu.offsetWidth || 200; // ancho estimado
-
-      // Alinear al lado derecho del botón
-      if (espacioRight < anchoMenu && rectaBtn.left > anchoMenu)
-        left = rectaBtn.right + window.scrollX - anchoMenu;
-
-      // Asegurar que no se salga de la ventana
-      top = Math.max(
-        10,
-        Math.min(top, window.scrollY + ventanaAlto - alturaMenu - 10),
-      );
-      left = Math.max(
-        10,
-        Math.min(left, window.scrollX + ventanaAncho - anchoMenu - 10),
-      );
-
-      this.menuStyle = `position: fixed; top: ${top}px; left: ${left}px;`;
-    }
-  }
-
-  function manejarTeclas() {
-    /** @type { KeyboardEvent } e */
-    const e = this.$event;
-    const $focus = this.$focus;
-
-    switch (e.key) {
-      case "Escape": {
-        e.stopPropagation();
-        this.abierto = false;
-        this.abiertoPorTeclado = false;
-        return this.$nextTick(() => this.$refs.trigger.focus());
+      // Si no hay espacio abajo pero sí arriba, mostrar arriba
+      if (
+        spaceBelow < dropdownRect.height &&
+        spaceAbove >= dropdownRect.height
+      ) {
+        verticalFlip = "top";
       }
-      case "ArrowDown": {
-        e.preventDefault();
-        return $focus.wrap().next();
+      // Si no hay espacio en ninguna dirección, mostrar donde haya más espacio
+      else if (
+        spaceBelow < dropdownRect.height &&
+        spaceAbove < dropdownRect.height
+      ) {
+        verticalFlip = spaceBelow >= spaceAbove ? "bottom" : "top";
       }
-      case "ArrowUp": {
-        e.preventDefault();
-        return $focus.wrap().previous();
+
+      // Determinar posición horizontal
+      let horizontalFlip = ""; // Por defecto a la izquierda
+      const spaceRight = window.innerWidth - buttonRect.left;
+      const spaceLeft = buttonRect.right;
+
+      // Si el dropdown es más ancho que el espacio a la izquierda, mostrar a la derecha
+      if (dropdownRect.width > spaceLeft && spaceRight >= dropdownRect.width) {
+        horizontalFlip = "right";
       }
-      case "Home": {
-        if (e.target.type !== "text") {
-          e.preventDefault();
-          return $focus.wrap().first();
-        }
+      // Si no cabe en ninguna dirección, mostrar donde haya más espacio
+      else if (
+        dropdownRect.width > spaceLeft &&
+        dropdownRect.width > spaceRight
+      ) {
+        horizontalFlip = spaceRight >= spaceLeft ? "right" : "left";
       }
-      case "End": {
-        if (e.target.type !== "text") {
-          e.preventDefault();
-          return $focus.wrap().last();
-        }
-      }
+
+      if (verticalFlip) {
+        if (verticalFlip === "top")
+          style += "bottom: 100%; margin-bottom: 8px;";
+        else if (verticalFlip === "bottom")
+          style += "top: 100%; margin-top: 8px;";
+      } else if (this.pos_x) style += this.pos_x;
+
+      if (horizontalFlip) {
+        if (horizontalFlip === "left") style += "right: 0;";
+        else if (horizontalFlip === "right") style += "left: 0;";
+      } else if (this.pos_y) style += this.pos_y;
+
+      if (style) log(style);
+
+      if (style) this.menuStyle = style;
     }
 
-    this.shiftPresionado = e.shiftKey;
+    // luego de la animación
+    else
+      this.posTimeout = setTimeout(
+        () => (this.menuStyle = "display: none; " + (this.posPorDefecto || "")),
+        200,
+      );
   }
-}
+};
